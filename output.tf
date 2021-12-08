94b6580db01254e58e8267567e95b4a6eb9c7393
Swathikarun <94472101+Swathikarun@users.noreply.github.com>
2021-12-09 02:00:45 +0530

    commit1

---
+++ output.tf
@@ -0,0 +1,17 @@
+output "vpc_id" {
+value = aws_vpc.vpc.id
+}
+
+output "subnet_public1_id" {
+value = aws_subnet.public1.id
+}
+
+output "subnet_public2_id" {
+value = aws_subnet.public1.id
+}
+
+output "subnet_private1_id" {
+value = aws_subnet.public1.id
+}
+
+
